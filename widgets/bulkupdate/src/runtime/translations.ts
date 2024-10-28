f9fbaacd96b3f27baf19a69a9a42edddcb79b660
Paul Haakma <phaakma@gmail.com>
2024-10-29 11:15:30 +1300

    Adding translation support

---
+++ widgets/bulkupdate/src/runtime/translations.ts
@@ -0,0 +1,9 @@
+export default {
+  widgetTitle: 'Bulk assign tasks',
+  numSelectedRecords: 'Number of selected records:',
+  selectionPlaceHolder: 'Please select a user',
+  buttonText: 'Assign tasks',
+  alertSuccess: 'All edits applied successfully.',
+  alertWarning: 'Some edits encountered errors.',
+  alertError: 'All edits encountered errors.'
+}
