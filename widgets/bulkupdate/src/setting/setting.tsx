6c3b68134c3aa34ad74cb2bd9192682b28f8c599
Paul Haakma <phaakma@gmail.com>
2024-10-30 14:01:50 +1300

    Added settings for controlling widget text for title, selector and button. Set data source selector to be mandatory.

--- widgets/bulkupdate/src/setting/setting.tsx
+++ widgets/bulkupdate/src/setting/setting.tsx
@@ -4,14 +4,13 @@ import {
   type IMFieldSchema,
   type UseDataSource,
   AllDataSourceTypes,
-  ImmutableArray,
-  type FeatureLayerDataSource,
-  type DataSource
+  type FeatureLayerDataSource
 } from 'jimu-core'
 import { type AllWidgetSettingProps } from 'jimu-for-builder'
+import { TextInput, Label } from 'jimu-ui'
 import { DataSourceSelector, FieldSelector } from 'jimu-ui/advanced/data-source-selector'
 import {
-  SettingRow
+  SettingSection
 } from 'jimu-ui/advanced/setting-components'
 import { DataSourceManager } from 'jimu-core'
 import { type IMConfig } from '../config'
@@ -22,32 +21,47 @@ export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
   const [hiddenFields, setHiddenFields] = useState(Immutable([]))
 
   useEffect(() => {
-    if (props.useDataSourcesEnabled && props.useDataSources && props.useDataSources.length > 0) {
+    if (props.useDataSources && props.useDataSources.length > 0) {
       const ds: FeatureLayerDataSource = DataSourceManager.getInstance().getDataSource(props.useDataSources[0].dataSourceId) as FeatureLayerDataSource
-      const layerDef = ds.getLayerDefinition()
-      const _hiddenFields = layerDef.fields
-        .filter(item =>
-          !item.editable ||
-          !item.domain ||
-          (item.domain && item.domain.type !== 'codedValue')
-        )
-        .map(item => item.name)
+      if (ds) {
+        const layerDef = ds.getLayerDefinition()
+        const _hiddenFields = layerDef.fields
+          .filter(item =>
+            !item.editable ||
+            !item.domain ||
+            (item.domain && item.domain.type !== 'codedValue')
+          )
+          .map(item => item.name)
 
-      setHiddenFields(Immutable(_hiddenFields))
+        setHiddenFields(Immutable(_hiddenFields))
+      }
     }
-  }, [props.useDataSourcesEnabled, props.useDataSources])
-
-  const onToggleUseDataEnabled = (useDataSourcesEnabled: boolean) => {
-    props.onSettingChange({
-      id: props.id,
-      useDataSourcesEnabled
-    })
-  }
+  }, [props.useDataSources])
 
   const onDataSourceChange = (useDataSources: UseDataSource[]) => {
     props.onSettingChange({
       id: props.id,
       useDataSources: useDataSources
+    })
+  }
+
+  const widgetTitleChangeHandler = (val: string) => {
+    props.onSettingChange({
+      id: props.id,
+      config: props.config.set('widgetTitle', val)
+    })
+  }
+  const valuePlaceholderChangeHandler = (val: string) => {
+    props.onSettingChange({
+      id: props.id,
+      config: props.config.set('valuePlaceHolder', val)
+    })
+  }
+
+  const buttonTextChangeHandler = (val: string) => {
+    props.onSettingChange({
+      id: props.id,
+      config: props.config.set('buttonText', val)
     })
   }
 
@@ -61,37 +75,41 @@ export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
   }
 
   return <div className='widget-setting-listen-selection-change p-2'>
-    <SettingRow
-      flow='wrap'
-      label='Set the feature layer source. This must be an editable layer.'
-    >
+    <SettingSection title='Configure data settings'>
+      <Label>Set the feature layer source. This must be an editable feature layer.</Label>
       <DataSourceSelector
         types={Immutable([AllDataSourceTypes.FeatureLayer])}
         useDataSources={props.useDataSources}
-        useDataSourcesEnabled={props.useDataSourcesEnabled}
-        onToggleUseDataEnabled={onToggleUseDataEnabled}
+        mustUseDataSource= {true}
         onChange={onDataSourceChange}
         widgetId={props.id}
       />
-    </SettingRow>
+      { props.useDataSources && props.useDataSources.length > 0 &&
+        <>
+          <Label className='pt-5' >
+            Select the field that the updates will be applied to. Valid fields must be editable and have a coded value domain.
+          </Label>
+          <FieldSelector
+            useDataSources={props.useDataSources}
+            onChange={fieldsListChangeHandler}
+            selectedFields={Immutable([props.config.calculateField])}
+            isMultiple={false}
+            isSearchInputHidden={false}
+            isDataSourceDropDownHidden
+            useDropdown={true}
+            hiddenFields={hiddenFields}
+          />
+        </>
+      }
+    </SettingSection>
 
-    {props.useDataSourcesEnabled && props.useDataSources && props.useDataSources.length > 0 &&
-
-      <SettingRow
-        flow='wrap'
-        label='Select the field that the updates will be applied to. Valid fields must be editable and have a coded value domain.'
-      >
-        <FieldSelector
-          useDataSources={props.useDataSources}
-          onChange={fieldsListChangeHandler}
-          selectedFields={Immutable([props.config.calculateField])}
-          isMultiple={false}
-          isSearchInputHidden={false}
-          isDataSourceDropDownHidden
-          useDropdown={true}
-          hiddenFields={hiddenFields}
-        />
-      </SettingRow>
-    }
+    <SettingSection title='Configure display settings'>
+      <Label>Widget title - displays at the top of the widget.</Label>
+      <TextInput type='text' defaultValue={props.config.widgetTitle} onAcceptValue={widgetTitleChangeHandler} />
+      <Label>Value placeholder text.</Label>
+      <TextInput type='text' defaultValue={props.config.valuePlaceHolder} onAcceptValue={valuePlaceholderChangeHandler} />
+      <Label>Button text.</Label>
+      <TextInput type='text' defaultValue={props.config.buttonText} onAcceptValue={buttonTextChangeHandler} />
+    </SettingSection>
   </div>
 }
