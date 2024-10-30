82a478d30bbcca337e31a3fc19276ef6ddcaba48
Paul Haakma <phaakma@gmail.com>
2024-10-30 14:02:23 +1300

    added new settings values

--- widgets/bulkupdate/src/config.ts
+++ widgets/bulkupdate/src/config.ts
@@ -2,6 +2,9 @@ import { type ImmutableObject } from 'seamless-immutable'
 
 export interface Config {
   calculateField: string
+  widgetTitle: string
+  valuePlaceHolder: string
+  buttonText: string
 }
 
 export type IMConfig = ImmutableObject<Config>
