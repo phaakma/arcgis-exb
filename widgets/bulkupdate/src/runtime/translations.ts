ca8e191fa2d642b91e0b6198f34f27a5248a8314
Paul Haakma <phaakma@gmail.com>
2024-10-30 14:02:08 +1300

    Added new setting values

--- widgets/bulkupdate/src/runtime/translations.ts
+++ widgets/bulkupdate/src/runtime/translations.ts
@@ -1,9 +1,9 @@
 export default {
   _widgetLabel: 'Bulk Update',
-  widgetTitle: 'Bulk assign tasks',
+  widgetTitle: 'Bulk Update Records',
   numSelectedRecords: 'Number of selected records:',
-  selectionPlaceHolder: 'Please select a user',
-  buttonText: 'Assign tasks',
+  selectionPlaceHolder: 'Please select value',
+  buttonText: 'Update records',
   alertSuccess: 'All edits applied successfully.',
   alertWarning: 'Some edits encountered errors.',
   alertError: 'All edits encountered errors.'
